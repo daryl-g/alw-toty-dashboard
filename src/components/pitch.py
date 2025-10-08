a995f7bd556beaac9d962bb690d0177562fa372d
Daryl <daohoang.thai@gmail.com>
2025-10-08 23:30:01 +1000

    ALW-28 & #64: Fix missing positions

--- src/components/pitch.py
+++ src/components/pitch.py
@@ -12,24 +12,16 @@ from utils import get_team_name, import_fonts
 @st.cache_resource
 def get_positions(
     _pitch: Pitch | VerticalPitch,
-    _ax: Axes,
-    team: str,
     second_striker: bool = False,
     line: int = 5,
-    node_size: float = 500,
-    text_size: int = 8,
 ) -> DataFrame:
     """
     Get positions coordinates on the pitch.
 
     Args:
         pitch (mplsoccer.Pitch | mplsoccer.VerticalPitch): mplsoccer Pitch object with specified pitch type.
-        ax (matplotlib.axes.Axes): The axis to plot on.
-        team (str): Name of team selected by the user.
         second_striker (bool, optional): Whether to include the second striker position. Defaults to False.
         line (int, optional): Number of lines in the formation (4 or 5). Defaults to 4.
-        node_size (float, optional): Size of the position nodes. Defaults to 1500.
-        text_size (int, optional): Font size of the position text. Defaults to 14.
 
     Returns:
         pd.DataFrame: Coordinates of the positions on the pitch, along with position nodes being plotted.
@@ -72,7 +64,37 @@ def get_positions(
         }
     )
 
+    return positions
+
+
+def plot_positions(
+    pitch: Pitch | VerticalPitch,
+    ax: Axes,
+    team: str,
+    second_striker: bool = False,
+    line: int = 5,
+    node_size: float = 500,
+    text_size: int = 8,
+) -> None:
+    """
+    Plot the positions on the pitch
+
+    Args:
+        pitch (mplsoccer.Pitch | mplsoccer.VerticalPitch): mplsoccer Pitch object with specified pitch type.
+        ax (matplotlib.axes.Axes): The axis to plot on.
+        team (str): Name of team selected by the user.
+        second_striker (bool, optional): Whether to include the second striker position. Defaults to False.
+        line (int, optional): Number of lines in the formation (4 or 5). Defaults to 4.
+        node_size (float, optional): Size of the position nodes. Defaults to 1500.
+        text_size (int, optional): Font size of the position text. Defaults to 14.
+
+    Returns:
+        pd.DataFrame: Coordinates of the positions on the pitch, along with position nodes being plotted.
+    """
+
     colours = get_team_colours(team=get_team_name(team, mode="short"))
+    # Get the position coordinates
+    positions = get_positions(_pitch=pitch, second_striker=second_striker, line=line)
 
     # Plot the position nodes on the pitch
     for i in range(len(positions.index)):
@@ -81,17 +103,17 @@ def get_positions(
             positions.loc[positions.index[i], "y"],
         )
 
-        _pitch.scatter(
+        pitch.scatter(
             positions_coords[0],
             positions_coords[1],
             s=node_size,
             color=colours["primary"],
             edgecolors=colours["secondary"],
             zorder=1,
-            ax=_ax,
+            ax=ax,
         )
 
-        _pitch.annotate(
+        pitch.annotate(
             positions.index[i],
             xy=positions_coords,
             xytext=positions_coords,
@@ -100,7 +122,5 @@ def get_positions(
             fontproperties=import_fonts(weight="bold"),
             fontsize=text_size,
             color=colours["secondary"],
-            ax=_ax,
+            ax=ax,
         )
-
-    return positions
